4e626ae52f719e3a4e5a9c382da0cfecc55fa97a
Muhammad Hardiansyah <71612996+muhammadhardiansyah@users.noreply.github.com>
2022-07-07 23:40:50 +0700

    Update rental_mobil.sql

--- rental_mobil.sql
+++ rental_mobil.sql
@@ -1,108 +1,145 @@
--- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: rental_mobil
--- ------------------------------------------------------
--- Server version	10.4.24-MariaDB
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 07 Jul 2022 pada 18.39
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `mobil`
+-- Database: `rental_mobil`
 --
 
-DROP TABLE IF EXISTS `mobil`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mobil`
+--
+
 CREATE TABLE `mobil` (
-  `id_mobil` int(255) NOT NULL AUTO_INCREMENT,
+  `id_mobil` int(255) NOT NULL,
   `merk` varchar(255) NOT NULL,
   `jenis` varchar(255) NOT NULL,
-  `no_polisi` varchar(10) NOT NULL,
-  PRIMARY KEY (`id_mobil`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `no_polisi` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `mobil`
+-- Dumping data untuk tabel `mobil`
 --
 
-LOCK TABLES `mobil` WRITE;
-/*!40000 ALTER TABLE `mobil` DISABLE KEYS */;
-INSERT INTO `mobil` VALUES (10,'Suzuki','SUV','DD5657KK');
-/*!40000 ALTER TABLE `mobil` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `mobil` (`id_mobil`, `merk`, `jenis`, `no_polisi`) VALUES
+(10, 'Suzuki', 'SUV', 'DD5657KK'),
+(12, 'BMW', 'Pribadi', 'AD7922GH'),
+(13, 'Cobalt', 'Minibus', 'AD7812KC'),
+(14, 'Avanza', 'Sedan', 'AD5912LK'),
+(15, 'Daihatsu', 'SUV', 'AD3670OP');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `peminjaman`
+-- Struktur dari tabel `peminjaman`
 --
 
-DROP TABLE IF EXISTS `peminjaman`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `peminjaman` (
-  `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT,
+  `id_peminjaman` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `id_mobil` int(11) NOT NULL,
   `tgl_peminjaman` varchar(255) NOT NULL,
   `tgl_kembali` varchar(255) NOT NULL,
-  `total_biaya` int(11) NOT NULL,
-  PRIMARY KEY (`id_peminjaman`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `total_biaya` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `peminjaman`
+-- Dumping data untuk tabel `peminjaman`
 --
 
-LOCK TABLES `peminjaman` WRITE;
-/*!40000 ALTER TABLE `peminjaman` DISABLE KEYS */;
-INSERT INTO `peminjaman` VALUES (18,1,10,'08-07-2022','09-07-2022',200000);
-/*!40000 ALTER TABLE `peminjaman` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `peminjaman` (`id_peminjaman`, `id_user`, `id_mobil`, `tgl_peminjaman`, `tgl_kembali`, `total_biaya`) VALUES
+(19, 2, 10, '08-07-2022', '09-07-2022', 200000),
+(20, 46, 12, '08-07-2022', '09-07-2022', 200000),
+(21, 47, 13, '15-07-2022', '17-07-2022', 300000),
+(22, 3, 14, '21-07-2022', '23-07-2022', 300000);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `nama` varchar(255) NOT NULL,
-  `akses` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `akses` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'user1','user1','user1','User'),(2,'user2','user2','user2','User'),(3,'user3','user3','user3','User'),(4,'admin','admin','admin','Admin');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama`, `akses`) VALUES
+(2, 'user2', 'user2', 'user2', 'User'),
+(3, 'user3', 'user3', 'user3', 'User'),
+(4, 'admin', 'admin', 'admin', 'Admin'),
+(46, 'user1', 'user1', 'user1', 'User'),
+(47, 'user4', 'user4', 'user4', 'User');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`id_mobil`);
+
+--
+-- Indeks untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_peminjaman`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `mobil`
+--
+ALTER TABLE `mobil`
+  MODIFY `id_mobil` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-07-05 21:06:59
